b9bf3b457765f74742c1b7966e4404ff5f5d24a5
Divyatej Mishra <76866090+DivyatejM@users.noreply.github.com>
2021-07-28 17:44:02 +0530

    Add files via upload

---
+++ 1_1_isUnique.cpp
@@ -0,0 +1,24 @@
+// Implement an algorithm to determine if a string has all unique characters.
+#include<bits/stdc++.h>
+using namespace std;
+int ascii[256];
+bool isUnique(string s) {
+  for(int i=0;i<s.length();i++) {
+    int val=int(s[i]);
+    if(ascii[val]) {
+      return false;
+    }
+    ascii[val] = true; 
+  }
+  return true;
+}
+int main() {
+  string s;
+  getline(cin,s);
+  if(isUnique(s))
+    cout<<"Unique";
+  else
+    cout<<"Not Unique";
+
+  return 0;
+}
